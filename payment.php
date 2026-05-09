1e4dde6278df47e4a02a6320893e7ff9a88945ee
ar209267 <rvai80054@gmail.com>
2026-05-09 16:07:50 +0600

    Add payment form for bKash/Nagad

---
+++ payment.php
@@ -0,0 +1,13 @@
+<form action="ssl-request.php" method="POST">
+
+<input type="number"
+name="amount"
+placeholder="Enter Amount">
+
+<button type="submit">
+
+Pay With bKash / Nagad
+
+</button>
+
+</form> 
